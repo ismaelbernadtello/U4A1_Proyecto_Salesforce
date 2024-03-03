6494dcf594b73f66462118f2e741f17f0f78b9bd
JesusRolandoMontero <montero.kijes20@zaragoza.salesianos.edu>
2024-03-03 21:00:18 +0100

    Guardar y recuperar peleas hecho

--- proyecto-boxeo/force-app/main/default/aura/desplegables_peleas/desplegables_peleasController.js
+++ proyecto-boxeo/force-app/main/default/aura/desplegables_peleas/desplegables_peleasController.js
@@ -11,16 +11,54 @@
             if(state === "SUCCESS"){
                 var response = response.getReturnValue(); 
                 component.set("v.listaBoxeadoresBD",response)
-                console.log('lo que devuelve la llamada a la base para recuperar ' + response);
+                console.log("Lista boxeadores recuperada correctamente");
             }else{
                 console.log(response.getError());
             }
         });
         $A.enqueueAction(getListaRegistros);
+
+
+        var getListaPeleas = component.get("c.getListaPeleas");
+
+        getListaPeleas.setCallback(this, function(response){
+            var state = response.getState();
+            if(state === "SUCCESS"){
+                var response = response.getReturnValue(); 
+                component.set("v.listaPeleas",response)
+                console.log("Lista peleas recuperada correctamente");
+            }else{
+                console.log(response.getError());
+            }
+        }
+        );
+        $A.enqueueAction(getListaPeleas);
+
     },
 
     guardarPelea : function(component, event, helper) {
-    
+        var insertarPelea = component.get("c.insertarPelea");
+
+        alert('Boxeador 1: ' + component.get("v.boxeador1"));
+        alert('Boxeador 2: ' + component.get("v.boxeador2"));
+        alert('Ganador: ' + component.get("v.ganador"));
+        
+        insertarPelea.setCallback(this, function(response){
+            var state = response.getState();
+            if(state === "SUCCESS"){
+                console.log('Pelea insertada correctamente');
+            }else{
+                console.log(response.getError());
+            }
+        });
+
+        insertarPelea.setParams({
+            "boxeador1": component.get("v.boxeador1"),
+            "boxeador2": component.get("v.boxeador2"),
+            "resultado": component.get("v.ganador")
+        });
+
+        $A.enqueueAction(insertarPelea);
     }
 
 })
