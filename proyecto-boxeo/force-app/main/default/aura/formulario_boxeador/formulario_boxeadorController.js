18559e7c6562115609d97206c27c46d4a4d77940
JesusRolandoMontero <montero.kijes20@zaragoza.salesianos.edu>
2024-03-03 18:50:54 +0100

    Guardar boxeadores en sesion funcionando y control del tipo de datos del formulario

--- proyecto-boxeo/force-app/main/default/aura/formulario_boxeador/formulario_boxeadorController.js
+++ proyecto-boxeo/force-app/main/default/aura/formulario_boxeador/formulario_boxeadorController.js
@@ -3,15 +3,35 @@
 
     },
     guardarBoxeador : function(component, event, helper) {
+        
+        // Obtener los valores de los campos desde la vista
+        var nombre = component.get("v.nombre");
+        var apellido = component.get("v.apellido");
+        var apodo = component.get("v.apodo");
+        var edad = component.get("v.edad");
+        var altura = component.get("v.altura");
+        var peso = component.get("v.peso");
+        
+
+        // Validar los tipos de datos
+        if (typeof nombre !== 'string' || typeof apellido !== 'string' || typeof apodo !== 'string' ||
+        isNaN(parseInt(edad)) || isNaN(parseInt(altura)) || isNaN(parseInt(peso))) {
+        // Si los tipos de datos no son correctos, mostrar un mensaje de error
+        // y abortar la operación
+        alert('Los datos ingresados no son del tipo correcto. Por favor, verifique los datos ingresados.');
+        return;
+        }
+
         var evt = $A.get("e.c:guardarBoxeadorEvent"); //Obtenemos el evento
-        evt.setParams({
-            nombre: component.get("v.nombre"),
-            apellido: component.get("v.apellido"),
-            apodo: component.get("v.apodo"),
-            edad : component.get("v.edad"),
-            peso : component.get("v.peso"),
-            altura : component.get("v.altura")
-        }); //Asignamos los valores del formulario al evento
+        
+        evt.setParams({ // Asignamos los valores del formulario al evento
+            nombre: nombre,
+            apellido: apellido,
+            apodo: apodo,
+            edad: edad,
+            peso: peso,
+            altura: altura
+        }); 
 
         console.log('nombre: ' + component.get("v.nombre") 
         + ' apellido: ' + component.get("v.apellido") 
--- proyecto-boxeo/force-app/main/default/aura/lista_boxeadores/lista_boxeadoresController.js
+++ proyecto-boxeo/force-app/main/default/aura/lista_boxeadores/lista_boxeadoresController.js
@@ -4,6 +4,41 @@
     },
 
     handleGuardarBoxeadorEvent : function(component, event, helper) {
-        console.log('Llega a: handleGuardarBoxeadorEvent');
+        // console.log('Llega a: handleGuardarBoxeadorEvent');
+        var nombre = event.getParam("nombre");
+        var apellido = event.getParam("apellido");
+        var apodo = event.getParam("apodo");
+        var edad = event.getParam("edad");
+        var peso = event.getParam("peso");
+        var altura = event.getParam("altura");
+
+        // console.log('nombre: ' + nombre
+        // + ' apellido: ' + apellido
+        // + ' apodo: ' + apodo
+        // + ' edad: ' + edad
+        // + ' peso: ' + peso
+        // + ' altura: ' + altura);
+
+
+        var boxeador = {
+            "nombre": nombre,
+            "apellido": apellido,
+            "apodo": apodo,
+            "edad": edad,
+            "peso": peso,
+            "altura": altura
+        };
+        //console.log('boxeador: ' + boxeador.nombre);
+        var listaBoxeadores = component.get("v.listaBoxeadores");
+        
+        if(listaBoxeadores.length == 0){ //Si es el primer objeto que se inserta en la lista, este se añade a la posición 0
+            listaBoxeadores[0] = boxeador;
+        }
+        else{
+            listaBoxeadores[(listaBoxeadores.length)] = boxeador;
+        }
+        
+        component.set("v.listaBoxeadores", listaBoxeadores);
+        //console.log('listaBoxeadores: ' + component.get("v.listaBoxeadores[0].nombre"));
     }
 })
